ac7bc9ae7ff65f5954d4c110c289b3398c9d3b1b
Петр Кленкин <pk@webpractik.ru>
2023-06-01 19:12:19 +0300

    fix(file): поиск файла

--- baseline/solution/implementation/solution_demo.py
+++ baseline/solution/implementation/solution_demo.py
@@ -2,8 +2,11 @@ from abc import ABC
 import asyncio
 import random
 import json
-from typing import Union
+import os.path
+import time
 
+from typing import Union
+from pathlib import Path
 from loguru import logger
 
 from baseline.tools.env_config import DEBUG_MAX_MARKUP_DEMO_DELAY, CHECK_DIRECTORY_ANSWER, CHECK_DIRECTORY_ANSWER_DELAY
@@ -38,6 +41,27 @@ class SolutionDemo(SolutionAbstract):
 
     async def execute_async(self) -> list[dict[str, Union[int, str]]]:
         await asyncio.sleep(random.randrange(0, DEBUG_MAX_MARKUP_DEMO_DELAY + 1))
+        self._logger.info('execute async runtime')
+        self._logger.info(f'env variables - CHECK_DIRECTORY_ANSWER = {CHECK_DIRECTORY_ANSWER}')
+        self._logger.info(f'env variables - CHECK_DIRECTORY_ANSWER_DELAY = {CHECK_DIRECTORY_ANSWER_DELAY}')
+
+        directory_conventional_name = f'files/sessions/{self.epicrisis.session_id}/output/{self.epicrisis.epicrisis_id}_{self.epicrisis.version_id}_{self.epicrisis.task_id}.json'
+
+
+        counter = 0
+        while not os.path.exists(directory_conventional_name) | (counter == 60):
+            self._logger.info(
+                f'while loop')
+            time.sleep(1)
+            counter += 1
+
+        if os.path.isfile(directory_conventional_name):
+            self._logger.info(f'Solution file found. {self.epicrisis.epicrisis_id}_{self.epicrisis.version_id}_{self.epicrisis.task_id}.json')
+        else:
+            raise ValueError("%s isn't a file!" % directory_conventional_name)
+
+
+
         return self.__execute()
 
     def execute(self) -> list[dict[str, Union[int, str]]]:
